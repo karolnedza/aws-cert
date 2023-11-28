a6dc289cf1cbdc4e8ae92d78335795ce43e1315e
Karol Nedza <karol@Karols-MacBook-Pro.local>
2023-11-28 12:03:09 +0100

    Cert only

--- main.tf
+++ main.tf
@@ -17,19 +17,19 @@ data "aws_route53_zone" "example" {
 }
 
 
-resource "aws_route53_record" "example" {
-  for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "example" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.example.zone_id
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.example.zone_id
+# }
